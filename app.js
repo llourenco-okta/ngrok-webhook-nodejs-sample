48ae3e351faab9061110e43fdf99b250a1d85406
Luis <lourenco.lou@gmail.com>
2024-04-17 15:22:10 +0200

    Test

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-var express = require('express');
-var app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const express = require("express");
+const app = express();
 
-const port = 3000;
+app.get("/", (req, res) => { res.send("Express on Vercel"); });
+const PORT = process.env.PORT || 5000;
 
-app.all('/*', function (req, res) {
-   console.log("-------------- New Request --------------");
-   console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
-   console.log("Body:"+ JSON.stringify(req.body, null, 3));
-   res.json({ message: "Thank you for the message" });
-})
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
-app.listen(port, function () {
-   console.log(`Example app listening at ${port}`)
-})
+module.exports = app; // Export the Express app
